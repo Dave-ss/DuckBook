ff82975e29d64f8715bf471dfa8d0e32ace64538
dave <>
2022-07-12 11:30:31 -0300

    atualizando funcionarios para funcionar com arquivo

--- bookduck/src/main/java/com/mycompany/bookduck/Funcionarios.java
+++ bookduck/src/main/java/com/mycompany/bookduck/Funcionarios.java
@@ -7,6 +7,11 @@ package com.mycompany.bookduck;
 import java.util.List;
 import java.util.ArrayList;
 import com.mycompany.bookduck.pessoa.Funcionario;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,6 +19,7 @@ import com.mycompany.bookduck.pessoa.Funcionario;
  */
 public class Funcionarios {
     private  List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+    
     
     public Funcionarios(){
         
@@ -44,5 +50,43 @@ public class Funcionarios {
             System.out.println(f.getName() + " " + f.getCargo());
  
     }
+ 
     
+    public void alterarArquivo() throws FileNotFoundException, IOException{
+        FileWriter file = new FileWriter("funcionarios.txt");
+        BufferedWriter objeto = new BufferedWriter(file);
+        for(Funcionario f : this.funcionarios){
+            objeto.write(f.getName());
+            objeto.append(",");
+            objeto.write(f.getCpf());
+            objeto.append(",");
+            objeto.write(f.getEmail());
+            objeto.append(",");
+            objeto.write(String.valueOf(f.getCargo()));
+            objeto.append(",");
+            objeto.write(String.valueOf(f.getSalario()));
+            objeto.append("=");
+        }
+        objeto.close();        
+    }
+    
+    public void carregarArquivo(Funcionarios funcionarios) throws FileNotFoundException, IOException, ClassNotFoundException{
+        try{
+            FileReader file = new FileReader("funcionarios.txt");
+            BufferedReader objeto = new BufferedReader(file);
+            StringBuilder content = new StringBuilder();
+
+            List<String> pessoas = new ArrayList<>(Arrays.asList((objeto.readLine()).split("=")));
+                        
+            for(String p : pessoas){
+                content.append(p);
+                ArrayList<String> myList;
+                myList = new ArrayList<>(Arrays.asList((p).split(",")));
+                Funcionario f = new Funcionario(myList.get(0), myList.get(1), myList.get(2), myList.get(3), myList.get(4));
+                addFuncionarios(f);
+            }
+        } catch(FileNotFoundException e){    
+            System.out.println("arquivo inexistente");
+        }
+    }
 }
