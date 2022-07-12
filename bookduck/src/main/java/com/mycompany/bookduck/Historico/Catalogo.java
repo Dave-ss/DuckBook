609e5cbbb8966394c332df4e9cd51315705b36f0
dave <>
2022-07-12 11:29:53 -0300

    atualizando menu

--- bookduck/src/main/java/com/mycompany/bookduck/Historico/Catalogo.java
+++ bookduck/src/main/java/com/mycompany/bookduck/Historico/Catalogo.java
@@ -5,6 +5,12 @@
 package com.mycompany.bookduck.Historico;
 
 import com.mycompany.bookduck.Obra.Obra;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.HashMap;
 
 /**
@@ -53,5 +59,44 @@ public class Catalogo {
                 aux = obra;  
         }     
         return aux;
+    }
+    
+    public void alterarArquivo() throws FileNotFoundException, IOException{
+        FileWriter file = new FileWriter("catalogo.txt");
+        BufferedWriter objeto = new BufferedWriter(file);
+        for(Obra o : this.catalogo.keySet()){
+            objeto.write(o.getTitulo());
+            objeto.append(",");
+            objeto.write(o.getDataPublicacao());
+            objeto.append(",");
+            objeto.write(o.getEditora());
+            objeto.append(",");
+            objeto.write(o.getLocalizacao());
+            objeto.append (",");
+            objeto.write(String.valueOf(catalogo.get(o)));
+            objeto.append("=");
+        }
+        objeto.close();        
+    }
+    
+    public void carregarArquivo(Catalogo catalogo) throws FileNotFoundException, IOException, ClassNotFoundException{
+        try{
+            FileReader file = new FileReader("catalogo.txt");
+            BufferedReader objeto = new BufferedReader(file);
+            StringBuilder content = new StringBuilder();
+
+            List<String> obras = new ArrayList<>(Arrays.asList((objeto.readLine()).split("=")));
+                        
+            for(String o : obras){
+                content.append(o);
+                ArrayList<String> myList;
+                myList = new ArrayList<>(Arrays.asList((o).split(",")));
+                Obra obra = new Obra(myList.get(0), myList.get(1), myList.get(2), myList.get(3));
+                int aux = Integer.valueOf(myList.get(4));
+                catalogo.adcObra(obra, aux);
+            }
+        } catch(FileNotFoundException e){    
+            System.out.println("arquivo inexistente");
+        }
     }
 }
